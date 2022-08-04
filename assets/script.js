bb854ccce2f7b1d4c56459d6fc89c5c6ef587940
Munnos <munnos@hotmail.co.uk>
2022-08-04 08:27:34 +0100

    finished code

--- assets/script.js
+++ assets/script.js
@@ -12,4 +12,34 @@ $(document).ready(function () {
 
     getfromStorage();
 
+    var currentDay = document.querySelector("#currentDay");
+    var userHour = moment().hour();
+    var m = moment();
+
+    currentDay.textContent = m.format("MMMM Do YYYY, h:mm:ss a");
+
+    function timeCheck() {
+        var rowforTime = document.querySelectorAll(".row");
     
+
+
+    rowforTime.forEach((element) => {
+        var rowTime = element.dataset.time;
+        var colourArea = element.querySelector(".col-8");
+
+        if (userHour === rowTime) {
+            colourArea.classList.add("present");
+        } else if (userHour < rowTime) {
+            colourArea.classList.add("future");
+        } else {
+            colourArea.classList.add("past");
+}})};
+
+    timeCheck();
+
+    $(".saveBtn").on("click", function () {
+        var textInput = $(this).siblings(".col-8").val();
+        var textTime = $(this).parent().data("time");
+        localStorage.setItem(textTime, textInput);
+    })})
+
